0b3f1e686327d068aa9f66d0849deefb1a7123fc
mohachi8 <maedoimitsuaki@icloud.com>
2024-11-04 19:12:53 +0900

    デモUIの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Box,
+  Button,
+  IconButton,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h3">PokeFile</Typography>
+      <Typography variant="h6">このポケモンのタイプは何でしょう？</Typography>
+      <Box sx={{ p: 2 }} />
+
+      <Stack direction="row" spacing={1} sx={{ alignItems: "center" }}>
+        <Typography variant="h6">ピカチュウ</Typography>
+        <IconButton>
+          <ChangeCircleIcon />
+        </IconButton>
+      </Stack>
+      <Box sx={{ p: 2 }} />
+      <Paper elevation={3} sx={{ p: 2, width: "fit-content" }}>
+        <Typography variant="h6">タイプ1</Typography>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained">でんき</Button>
+          <Button variant="contained">くさ</Button>
+          <Button variant="contained">ほのお</Button>
+          <Button variant="contained">みず</Button>
+        </Stack>
+      </Paper>
+      <Box sx={{ p: 2 }} />
+      <Paper elevation={3} sx={{ p: 2, width: "fit-content" }}>
+        <Typography variant="h6">タイプ2</Typography>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained">でんき</Button>
+          <Button variant="contained">くさ</Button>
+          <Button variant="contained">ほのお</Button>
+          <Button variant="contained">みず</Button>
+        </Stack>
+      </Paper>
+      <Box sx={{ p: 2 }} />
+      <Button variant="contained">答え合わせ</Button>
+    </Box>
+  );
 }
 
-export default App
+export default App;
