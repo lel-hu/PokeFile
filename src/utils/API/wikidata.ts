a81cfb9fe1d3df479022dd4c01bff75b283470c5
Rengthgen2 <rength014@gmail.com>
2024-11-12 13:37:53 +0900

    タイプを取得できるように修正

--- src/utils/API/wikidata.ts
+++ src/utils/API/wikidata.ts
@@ -6,8 +6,8 @@ class Wikidata {
   private client = new SparqlClient({
     endpointUrl: this.endpointUrl,
   });
-  private pokemons: { ja: string; en: string }[] = []; // ポケモン情報を格納する配列
-  async fetchAllPokemonName(): Promise<{ ja: string; en: string }[]> {
+  private pokemons: { ja: string; en: string; type1: string; type2: string }[] = []; // ポケモン情報を格納する配列
+  async fetchAllPokemonName(): Promise<{ ja: string; en: string; type1: string; type2: string }[]> {
     if (this.pokemons.length > 0) {
       console.log("Pokemons already fetched");
       return this.pokemons;
@@ -20,17 +20,33 @@ class Wikidata {
     PREFIX wikibase: <http://wikiba.se/ontology#>
     PREFIX bd: <http://www.bigdata.com/rdf#>
 
-    SELECT DISTINCT ?pokemonLabel ?pokemonEnLabel WHERE {
+    SELECT DISTINCT ?pokemonLabel ?pokemonEnLabel ?typeLabel1 ?typeLabel2 WHERE {
+      # ポケモンのインスタンス
       ?pokemon wdt:P31/wdt:P279* wd:Q3966183.
-
-      FILTER EXISTS {
-        ?pokemon rdfs:label ?pokemonLabel.
-        FILTER(LANG(?pokemonLabel) = "ja")
-      }
-
+ 
+      # 日本語と英語のポケモン名を取得
+      ?pokemon rdfs:label ?pokemonLabel.
+        
+      FILTER(LANG(?pokemonLabel) = "ja")
       ?pokemon rdfs:label ?pokemonEnLabel.
       FILTER(LANG(?pokemonEnLabel) = "en")
-
+      # ファーストタイプを取得
+      OPTIONAL {
+        ?pokemon p:P31 ?typeStatement1.
+        ?typeStatement1 ps:P31 ?type1;
+                    pq:P518 wd:Q25931659.  # ファーストタイプ
+        ?type1 rdfs:label ?typeLabel1.
+        FILTER(LANG(?typeLabel1) = "ja")
+      }
+      # セカンドタイプを取得
+      OPTIONAL {
+        ?pokemon p:P31 ?typeStatement2.
+        ?typeStatement2 ps:P31 ?type2;
+                    pq:P518 wd:Q25931668.  # セカンドタイプ
+        ?type2 rdfs:label ?typeLabel2.
+        FILTER(LANG(?typeLabel2) = "ja")
+      }
+ 
       SERVICE wikibase:label { bd:serviceParam wikibase:language "ja". }
     }
     ORDER BY ?pokemonLabel
@@ -44,11 +60,15 @@ class Wikidata {
           console.log(
             "Data received:",
             row.pokemonLabel.value,
-            row.pokemonEnLabel.value
+            row.pokemonEnLabel.value,
+            row.typeLabel1.value,
+            row.typeLabel2.value
           ); // データが受信されたことを確認
           this.pokemons.push({
             ja: row.pokemonLabel.value,
             en: row.pokemonEnLabel.value,
+            type1: row.typeLabel1.value,
+            type2: row.typeLabel2.value,
           }); // ポケモン情報を配列に追加
           hasResults = true;
         });
@@ -74,8 +94,8 @@ class Wikidata {
     }
   }
 
-  async fetchRandomPokemon(): Promise<{ ja: string; en: string }> {
-    const pokemons: { ja: string; en: string }[] =
+  async fetchRandomPokemon(): Promise<{ ja: string; en: string; type1: string; type2: string }> {
+    const pokemons: { ja: string; en: string; type1: string; type2: string }[] =
       await this.fetchAllPokemonName();
     const randomPokemon = pokemons[Math.floor(Math.random() * pokemons.length)];
     console.log("Random Pokemon:", randomPokemon);
